b97cf819823552f121066997246cf6a00474b158
MargaridaTeles <telesmaegarida@gmail.com>
2023-04-28 14:17:55 +0100

    Criar algumas esferas e imprimir algumas enquanto umas vão de um lado para o outro e rebentas outras

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,7 +6,31 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Sphere[] spheres = new Sphere[3]
+            {
+                new Sphere(new Color(255, 0, 0, 255), 1.0f),
+                new Sphere(new Color(0, 255, 0, 255), 5.0f),
+                new Sphere(new Color(0, 0, 255, 255), 2.5f)
+            };
+
+            
+            Console.WriteLine("\nSPHERES:");
+            foreach (Sphere sphere in spheres)
+                Console.WriteLine(sphere);
+
+            spheres[0].Throw();
+            spheres[1].Throw();
+            spheres[1].Pop();
+            spheres[2].Throw();
+            spheres[2].Throw();
+
+            Console.WriteLine("\nplay around with spheres\n");
+
+            Console.WriteLine("SPHERES:");
+            foreach (Sphere sphere in spheres)
+                Console.WriteLine(sphere);
+            
         }
     }
 }

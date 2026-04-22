1fc01dc30d6d17f9c11b195d80e6d1b845ddedd7
MargaridaTeles <telesmaegarida@gmail.com>
2026-04-22 18:54:56 +0100

    Add values for RGBA

---
+++ ColorSpheres/Color.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+    }
+}

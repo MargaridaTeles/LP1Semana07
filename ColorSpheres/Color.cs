e5c8277a659b62415d7d97af20408d0fd8565873
MargaridaTeles <telesmaegarida@gmail.com>
2026-04-22 18:56:50 +0100

    Add constructors

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -8,5 +8,21 @@ namespace ColorSpheres
         private byte green;
         private byte blue;
         private byte alpha;
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+        
+        public Color(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            alpha = 255;
+        }
     }
 }

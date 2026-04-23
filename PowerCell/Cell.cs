6c3313cd08fd5f908115ff3db320fef03148eb67
MargaridaTeles <telesmaegarida@gmail.com>
2026-04-23 15:48:44 +0100

    Add a new class Cell

---
+++ PowerCell/Cell.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PowerCell
+{
+    public class Cell
+    {
+        private float charge;
+        private string Name { get; }
+        private float Charge;
+        private readonly int Level;
+
+        public override string ToString()
+        {
+            return $"[Name] Level {Level}: {Charge}/200";
+        }
+
+    }
+}
